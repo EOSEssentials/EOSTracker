7fad95a97c79611308d674a28b0f1aa519c310b7
Cesar Rodriguez <kesarr@gmail.com>
2018-06-03 23:03:19 +0200

    🚀ready for mainnet✨

--- src/app/components/transaction/transaction.component.ts
+++ src/app/components/transaction/transaction.component.ts
@@ -3,6 +3,7 @@ import {HttpClient} from '@angular/common/http';
 import {ActivatedRoute} from '@angular/router';
 import {environment} from '../../../environments/environment';
 import {EosService} from '../../services/eos.service';
+import {Subscription} from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-transaction',
@@ -11,7 +12,10 @@ import {EosService} from '../../services/eos.service';
 export class TransactionComponent implements OnInit {
   public id: string;
   public transaction = null;
+  public actions = null;
   public transactionRaw = null;
+  private subscriber: Subscription;
+  page = 1;
 
   constructor(private route: ActivatedRoute, private http: HttpClient, private eosService: EosService) {
   }
@@ -22,10 +26,20 @@ export class TransactionComponent implements OnInit {
     this.http.get(environment.apiUrl + '/transactions/' + this.id).subscribe(data => {
       this.transaction = data;
       console.log(this.transaction);
+
+      this.subscriber = this.route.queryParams.subscribe(params => {
+        this.page = params['page'] || 1;
+
+        this.http.get(environment.apiUrl + '/transactions/' + this.id + '/actions?' + 'page=' + this.page).subscribe(data => {
+          this.actions = data;
+          console.log(data);
+        });
+      });
+
     });
 
-    this.eosService.eos.getTransaction(this.id).then(result => {
+    /*this.eosService.eos.getTransaction(this.id).then(result => { TODO: not working
       this.transactionRaw = result;
-    });
+    });*/
   }
 }
