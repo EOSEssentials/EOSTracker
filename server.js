fe642689e682df4caaf773cb84be9219f70d882c
Cesar Rodriguez <kesarr@gmail.com>
2018-06-17 13:04:19 +0200

    concurrency

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
-//Install express server
 const express = require('express');
 const path = require('path');
-
+const throng = require('throng');
+const WORKERS = process.env.WEB_CONCURRENCY || 1;
+throng({workers: WORKERS, lifetime: Infinity}, start);
 const app = express();
-
-// Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
-
 app.get('/*', function(req,res) {
-
     res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
-
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
