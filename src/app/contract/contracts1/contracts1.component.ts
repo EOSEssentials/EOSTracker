005a34650d109affd396ef087528a87c89d3a9c5
Tommy Jiang <tjiang@NetOpss-iMac.local>
2018-06-28 16:40:49 -0700

    responsive contract table columns

--- src/app/contract/contracts1/contracts1.component.ts
+++ src/app/contract/contracts1/contracts1.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { MediaService } from '@angular/flex-layout';
 import { ActionService } from '../../services/action.service';
 import { Action } from '../../models/Action';
-import { Observable } from 'rxjs';
-import { switchMap, map, share } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { switchMap, map, share, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   templateUrl: './contracts1.component.html',
@@ -11,22 +12,21 @@ import { switchMap, map, share } from 'rxjs/operators';
 })
 export class Contracts1Component implements OnInit {
 
-  columnHeaders = [
-    'actionId',
-    'transactionId',
-    'createdAt',
-    'authorization',
-    'handler',
-    'name'
-  ];
+  columnHeaders$: Observable<string[]> = of(CONTRACT_COLUMNS);
   actions$: Observable<Action[]>;
 
   constructor(
     private route: ActivatedRoute,
+    private mediaService: MediaService,
     private actionService: ActionService
   ) { }
 
   ngOnInit() {
+    this.columnHeaders$ = this.mediaService.asObservable().pipe(
+      map(media => media.mqAlias),
+      distinctUntilChanged(),
+      map(mqAlias => mqAlias === 'xs' ? CONTRACT_COLUMNS.filter(c => c !== 'transactionId') : CONTRACT_COLUMNS)
+    );
     this.actions$ = this.route.queryParams.pipe(
       map(queryParams => queryParams.page ? Number(queryParams.page) : 1),
       switchMap(page => this.actionService.getActions(page)),
@@ -35,3 +35,12 @@ export class Contracts1Component implements OnInit {
   }
 
 }
+
+export const CONTRACT_COLUMNS = [
+  'actionId',
+  'transactionId',
+  'createdAt',
+  'authorization',
+  'handler',
+  'name'
+];
