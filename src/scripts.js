a2fa48ad05fce0aad41bfc9eacc9050ee0e8ea2d
Tommy Jiang <tianyangj@gmail.com>
2018-07-07 00:47:57 -0700

    remove jquery and bootstrap

--- src/scripts.js
+++ src/scripts.js
@@ -1,9 +1,1 @@
-$(function () {
-    $('#side-menu').metisMenu({toggle: false});
-})
 
-$(function () {
-    $(window).bind('load resize', function () {
-        $('div.navbar-collapse').addClass('collapse')
-    })
-})
