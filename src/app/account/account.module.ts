9bc003515ac56001e1d187e888b667643edab994
Tommy Jiang <tianyangj@gmail.com>
2018-08-22 01:40:25 -0700

    NgxChartsModule

--- src/app/account/account.module.ts
+++ src/app/account/account.module.ts
@@ -4,6 +4,7 @@ import { AccountRoutingModule } from './account-routing.module';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 import { AccountsComponent } from './accounts/accounts.component';
 import { AccountComponent } from './account/account.component';
@@ -19,7 +20,8 @@ import { CustomTokensComponent } from './account/custom-tokens/custom-tokens.com
     AccountRoutingModule,
     MatDialogModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    NgxChartsModule
   ],
   declarations: [
     AccountsComponent,
