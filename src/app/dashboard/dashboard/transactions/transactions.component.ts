ab93cfd74d2baf8e24d8c131b6e6f5e58a40b0d0
Tommy Jiang <tianyangj@gmail.com>
2018-08-28 21:30:55 -0700

    dashboard mobile view

--- src/app/dashboard/dashboard/transactions/transactions.component.ts
+++ src/app/dashboard/dashboard/transactions/transactions.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { AppService } from '../../../services/app.service';
 
@@ -9,23 +11,35 @@ import { AppService } from '../../../services/app.service';
 })
 export class TransactionsComponent implements OnInit {
 
-  transactionsColumns = [
-    'id',
-    'block_num',
-    'cpu',
-    'net',
-    'actions'
-  ];
+  columnHeaders$: Observable<string[]> = of(DEFAULT_HEADERS);
   transactions$;
 
   constructor(
+    private breakpointObserver: BreakpointObserver,
     private appService: AppService
   ) { }
 
   ngOnInit() {
+    this.columnHeaders$ = this.breakpointObserver.observe(Breakpoints.XSmall).pipe(
+      map(result => result.matches ? XSMALL_HEADERS : DEFAULT_HEADERS)
+    );
     this.transactions$ = this.appService.recentTransactions$.pipe(
       map(transactions => transactions.slice(0, 50))
     );
   }
 
 }
+
+const DEFAULT_HEADERS = [
+  'id',
+  'block_num',
+  'cpu',
+  'net',
+  'actions'
+];
+
+const XSMALL_HEADERS = [
+  'id',
+  'block_num',
+  'actions'
+];
