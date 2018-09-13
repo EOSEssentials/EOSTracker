c75f6eb72096892d9770423905722b547b06a6f0
Cesar Rodriguez <kesarr@gmail.com>
2018-09-13 14:31:33 +0200

    🚀

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,6 @@ export const environment = {
   votingUrl: 'https://eosportal.io',
   appName: 'EOS Tracker',
   logoUrl: '/assets/logo.png',
-  blockchainUrl: 'https://api1.eosdublin.io',
+  blockchainUrl: 'https://eos.greymass.com',
   chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906'
 };
