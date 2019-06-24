289aa4da25805ee723f54f891472006053ec3e0e
Hugo <hg.campanella@gmail.com>
2019-06-24 22:01:00 +0900

    Change Explorer Name

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   production: false,
   walletUrl: 'https://walleteos.com',
   votingUrl: 'https://eosportal.io',
-  appName: 'EOS Tracker',
+  appName: 'Liberland Explorer',
   logoUrl: '/assets/logo.png',
   blockchainUrl: 'https://proxy.eosnode.tools',
   chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
